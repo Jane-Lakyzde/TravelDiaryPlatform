3d4fcf634eb4ecc6349047510a88b9902c7f247a
GOK <janelakyzde@gmail.com>
2025-05-11 14:32:52 +0800

    change tabbar logo

---
+++ app/rednote/unpackage/dist/dev/mp-weixin/app.js
@@ -0,0 +1,52 @@
+"use strict";
+Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
+const common_vendor = require("./common/vendor.js");
+if (!Math) {
+  "./pages/login/login.js";
+  "./pages/login/phone.js";
+  "./pages/user/home.js";
+  "./pages/user/find.js";
+  "./pages/user/my.js";
+  "./pages/user/post.js";
+  "./pages/user/msg.js";
+  "./pages/admin/admin.js";
+  "./pages/admin/approve.js";
+  "./pages/admin/reject.js";
+  "./pages/admin/pending.js";
+  "./pages/sidebar/discover-friends/index.js";
+  "./pages/sidebar/creator-center/index.js";
+  "./pages/sidebar/drafts/index.js";
+  "./pages/sidebar/comments/index.js";
+  "./pages/sidebar/history/index.js";
+  "./pages/sidebar/orders/index.js";
+  "./pages/sidebar/cart/index.js";
+  "./pages/sidebar/wallet/index.js";
+  "./pages/sidebar/miniapp/index.js";
+  "./pages/sidebar/community-guidelines/index.js";
+  "./pages/sidebar/scan/index.js";
+  "./pages/sidebar/support/index.js";
+  "./pages/sidebar/settings/index.js";
+  "./pages/agreement/user-agreement.js";
+  "./pages/agreement/privacy-policy.js";
+  "./pages/agreement/buyer-notice.js";
+}
+const _sfc_main = {
+  onLaunch: function() {
+    common_vendor.index.__f__("log", "at App.vue:4", "App Launch");
+  },
+  onShow: function() {
+    common_vendor.index.__f__("log", "at App.vue:7", "App Show");
+  },
+  onHide: function() {
+    common_vendor.index.__f__("log", "at App.vue:10", "App Hide");
+  }
+};
+function createApp() {
+  const app = common_vendor.createSSRApp(_sfc_main);
+  return {
+    app
+  };
+}
+createApp().app.mount("#app");
+exports.createApp = createApp;
+//# sourceMappingURL=../.sourcemap/mp-weixin/app.js.map
---
+++ app/rednote/unpackage/dist/dev/mp-weixin/pages/login/phone.js
@@ -0,0 +1,86 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const api_user = require("../../api/user.js");
+const _sfc_main = {
+  __name: "phone",
+  setup(__props) {
+    const showHelp = common_vendor.ref(false);
+    const checked = common_vendor.ref(false);
+    const phone = common_vendor.ref("");
+    const password = common_vendor.ref("");
+    const areaCodes = common_vendor.ref(["+86", "+852", "+853", "+886"]);
+    const areaIndex = common_vendor.ref(0);
+    common_vendor.ref(false);
+    const goBack = () => {
+      common_vendor.index.navigateBack();
+    };
+    const toggleHelp = () => {
+      showHelp.value = !showHelp.value;
+    };
+    const onAreaChange = (e) => {
+      areaIndex.value = e.detail.value;
+    };
+    const goPwdLogin = () => {
+      common_vendor.index.showToast({ title: "请用手机号+密码登录", icon: "none" });
+    };
+    const canSubmit = common_vendor.computed(() => {
+      return phone.value.length === 11 && password.value.length >= 6 && checked.value;
+    });
+    const onConfirm = async () => {
+      if (phone.value.length !== 11) {
+        common_vendor.index.showToast({ title: "请输入11位手机号", icon: "none" });
+        return;
+      }
+      if (password.value.length < 6) {
+        common_vendor.index.showToast({ title: "密码长度至少6位", icon: "none" });
+        return;
+      }
+      if (!checked.value) {
+        common_vendor.index.showToast({ title: "请勾选协议", icon: "none" });
+        return;
+      }
+      if (!canSubmit.value)
+        return;
+      try {
+        const res = await api_user.phoneLogin({
+          phone: phone.value,
+          password: password.value
+        });
+        common_vendor.index.setStorageSync("token", res.token);
+        common_vendor.index.showToast({ title: "登录成功", icon: "success" });
+        common_vendor.index.reLaunch({ url: "/pages/user/home" });
+      } catch (err) {
+        common_vendor.index.showToast({ title: err.message || "登录失败", icon: "none" });
+        common_vendor.index.__f__("error", "at pages/login/phone.vue:97", "登录错误：", err);
+      }
+    };
+    return (_ctx, _cache) => {
+      return common_vendor.e({
+        a: common_vendor.o(goBack),
+        b: common_vendor.o(toggleHelp),
+        c: showHelp.value
+      }, showHelp.value ? {
+        d: common_vendor.o(() => {
+        })
+      } : {}, {
+        e: common_vendor.t(areaCodes.value[areaIndex.value]),
+        f: areaCodes.value,
+        g: areaIndex.value,
+        h: common_vendor.o(onAreaChange),
+        i: phone.value,
+        j: common_vendor.o(($event) => phone.value = $event.detail.value),
+        k: password.value,
+        l: common_vendor.o(($event) => password.value = $event.detail.value),
+        m: !canSubmit.value,
+        n: common_vendor.o(onConfirm),
+        o: common_vendor.o(goPwdLogin),
+        p: checked.value
+      }, checked.value ? {} : {}, {
+        q: checked.value ? 1 : "",
+        r: common_vendor.o(($event) => checked.value = !checked.value)
+      });
+    };
+  }
+};
+wx.createPage(_sfc_main);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/login/phone.js.map
